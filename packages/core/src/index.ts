463825c370e8420b76cdd096a79502e380c8efcd
Igor Kazhdan <igork3010@gmail.com>
2026-05-03 14:26:38 +0200

    feat(core): add public API exports and JSON Schema for editor support

---
+++ packages/core/src/index.ts
@@ -0,0 +1,25 @@
+export type {
+  GraphNode,
+  GraphEdge,
+  ContextModel,
+  ContextModelNode,
+  ProjectConfig,
+  PresetDefinition,
+  EdgeRule,
+} from './schema.js';
+
+export {
+  EVENT_MODELING_NODE_TYPES,
+  EVENT_MODELING_EDGE_TYPES,
+  GENERIC_NODE_TYPES,
+  GENERIC_EDGE_TYPES,
+  qualifiedId,
+  parseQualifiedId,
+} from './schema.js';
+
+export { EventGraph } from './graph.js';
+export { loadProject, loadContext, loadConfig, loadContextIntoGraph, findProjectDir } from './parser.js';
+export { QueryEngine } from './query.js';
+export { analyzeImpact, type ImpactResult, type ImpactOptions } from './impact.js';
+export { validateGraph, loadPreset, type ValidationError } from './validate.js';
+export { addNodeToContext, addEdgeToContext, removeNodeFromContext, generateYamlDiff, type DiffChanges } from './writer.js';
